30b71f1f17772edd0a551ec5540ed721afe14d51
zubedaa <zn63018n@pace.edu>
2022-04-01 11:36:47 -0400

    US5/US16 completed

--- TechCafe/app/src/main/java/com/example/techcafe/home_page.java
+++ TechCafe/app/src/main/java/com/example/techcafe/home_page.java
@@ -1,14 +1,35 @@
 package com.example.techcafe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class home_page extends AppCompatActivity {
+
+    private Button sd;
+    private Button cs;
+    private Button ur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        // Setting up each button to load their respective US20 activity
+        sd = (Button) findViewById(R.id.sd);
+        sd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loadUS20 = new Intent(home_page.this, sd_US20.class);
+                home_page.this.startActivity(loadUS20);
+            }
+
+
+        });
     }
 }
