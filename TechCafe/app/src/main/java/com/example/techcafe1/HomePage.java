614f25a94572e46798259103a7bed83c1efc2029
tumarkuss <tumarkuss@gmail.com>
2022-04-09 05:28:37 -0400

    Homepage, Tool Bar + nofications/profile icons

--- TechCafe/app/src/main/java/com/example/techcafe1/HomePage.java
+++ TechCafe/app/src/main/java/com/example/techcafe1/HomePage.java
@@ -4,40 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import java.util.zip.Inflater;
 
 public class HomePage extends AppCompatActivity {
-    Button user_logout_button;
-    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        mAuth=FirebaseAuth.getInstance();
 
-        Button user_logout_button = (Button) findViewById(R.id.button_signout);//button name is called, but XML ID is used.
-        user_logout_button.setOnClickListener(view -> {
-            mAuth.signOut();
-            Intent intent = new Intent(this, MainActivity.class);//class is the JAVA file, that calls XML within.
+        ImageButton open_profile_page = findViewById(R.id.profile_button);
+        open_profile_page.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ProfilePage.class);//class is the JAVA file, that calls XML within.
             startActivity(intent);
-            Toast.makeText(HomePage.this, "Logged out successfully.",
-                    Toast.LENGTH_SHORT).show();
         });
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser user = mAuth.getCurrentUser();
-        if(user == null){
-            Intent intent = new Intent(this, MainActivity.class);//class is the JAVA file, that calls XML within.
+
+        ImageButton open_notifications_page = findViewById(R.id.notifications_button);
+        open_notifications_page.setOnClickListener(view -> {
+            Intent intent = new Intent(this, NotificationsPage.class);//class is the JAVA file, that calls XML within.
             startActivity(intent);
-        }
+        });
+
+        View block1 = findViewById(R.id.block1);
+        block1.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SoftwareDevPage.class);//class is the JAVA file, that calls XML within.
+            startActivity(intent);
+        });
+
+        View block2 = findViewById(R.id.block2);
+        block2.setOnClickListener(view -> {
+            Intent intent = new Intent(this, CyberSecurityPage.class);//class is the JAVA file, that calls XML within.
+            startActivity(intent);
+        });
+
+        View block3 = findViewById(R.id.block3);
+        block3.setOnClickListener(view -> {
+            Intent intent = new Intent(this, UI_UXPage.class);//class is the JAVA file, that calls XML within.
+            startActivity(intent);
+        });
+
+
     }
 }
---
+++ TechCafe/app/src/main/java/com/example/techcafe1/SoftwareDevPage.java
@@ -0,0 +1,14 @@
+package com.example.techcafe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoftwareDevPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_software_dev_page);
+    }
+}
