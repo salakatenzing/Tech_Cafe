369cce78128f34c600112d7c663c614a738ebabc
tumarkuss <tumarkuss@gmail.com>
2022-04-10 18:54:19 -0400

    unnecessary files delete

--- TechCafe/app/src/main/java/com/example/techcafe1/MainActivity.java
+++ TechCafe/app/src/main/java/com/example/techcafe1/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
