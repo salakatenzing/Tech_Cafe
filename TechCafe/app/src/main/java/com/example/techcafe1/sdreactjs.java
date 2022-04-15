e5e24fe5d8c7495279db74dd6175d0d79406188b
Hamsira <hamsira@gmail.com>
2022-04-14 22:37:20 -0400

    us13

---
+++ TechCafe/app/src/main/java/com/example/techcafe1/sdreactjs.java
@@ -0,0 +1,14 @@
+package com.example.techcafe1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class sdreactjs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sdscreening2);
+    }
+}
---
+++ TechCafe/app/src/main/java/com/example/techcafe1/uiuxbehavioral.java
@@ -0,0 +1,14 @@
+package com.example.techcafe1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class uiuxbehavioral extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sdscreening2);
+    }
+}
