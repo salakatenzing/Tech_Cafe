8350e385c22801f4905694d3e0b98ba5417bc3a1
Tatiana <tanushka.z2010@gmail.com>
2022-04-17 00:16:38 -0400

    New UI and added Hamsira interview pages

---
+++ TechCafe/app/src/main/java/com/example/techcafe1/SDJavaScript.java
@@ -0,0 +1,14 @@
+package com.example.techcafe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SDJavaScript extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sdjava_script);
+    }
+}
