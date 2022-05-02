c75db291d4271835565504cf0f609dcd399852da
Tenzing Salaka <salakatenzing@gmail.com>
2022-05-02 10:26:48 -0400

    React resource for UI/UX

--- TechCafe/app/src/main/java/com/example/techcafe1/UIX_Resource_5.java
+++ TechCafe/app/src/main/java/com/example/techcafe1/UIX_Resource_5.java
@@ -3,12 +3,43 @@ package com.example.techcafe1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class UIX_Resource_5 extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class UIX_Resource_5 extends YouTubeBaseActivity {
+
+    Button reactButton;
+    YouTubePlayerView javaPlayerview;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uix_resource5);
+
+        reactButton = (Button) findViewById(R.id.React_play);
+        javaPlayerview = (YouTubePlayerView) findViewById(R.id.React_vid);
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("bMknfKXIFA8");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        reactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                javaPlayerview.initialize("AIzaSyCCEvFjfz4GFYUOpTaLl-gHbpHAtzSxgHg", onInitializedListener);
+            }
+        });
     }
 }
