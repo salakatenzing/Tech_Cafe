1b1d8a0e37a5ff3d4f1233f491e7a952730ae74d
Tenzing Salaka <salakatenzing@gmail.com>
2022-05-02 08:51:59 -0400

    values for topics

--- TechCafe/app/src/main/java/com/example/techcafe1/UIX_Skill_4.java
+++ TechCafe/app/src/main/java/com/example/techcafe1/UIX_Skill_4.java
@@ -8,12 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-public class UIX_Skill_4 extends AppCompatActivity {
+public class UIX_Skill_4 extends YouTubeBaseActivity {
     TextView sdlink3;
+    Button Fe4Video;
+    YouTubePlayerView javaPlayerview;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,27 @@ public class UIX_Skill_4 extends AppCompatActivity {
         setContentView(R.layout.activity_uix_skill4);
         sdlink3 = findViewById(R.id.sd_datastruc_res);
         sdlink3.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Fe4Video = (Button) findViewById(R.id.FE_skill4_play);
+        javaPlayerview = (YouTubePlayerView) findViewById(R.id.FE_4_vid);
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("RBSGKlAvoiM");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        Fe4Video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                javaPlayerview.initialize("AIzaSyCCEvFjfz4GFYUOpTaLl-gHbpHAtzSxgHg", onInitializedListener);
+            }
+        });
 
     }
 }
--- TechCafe/app/src/main/java/com/example/techcafe1/UIX_Skill_5.java
+++ TechCafe/app/src/main/java/com/example/techcafe1/UIX_Skill_5.java
@@ -3,12 +3,43 @@ package com.example.techcafe1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class UIX_Skill_5 extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class UIX_Skill_5 extends YouTubeBaseActivity {
+    Button Fe5Video;
+    YouTubePlayerView javaPlayerview;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uix_skill5);
+
+        Fe5Video = (Button) findViewById(R.id.FE_skill5_play);
+        javaPlayerview = (YouTubePlayerView) findViewById(R.id.FE_5_vid);
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("8hly31xKli0");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        Fe5Video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                javaPlayerview.initialize("AIzaSyCCEvFjfz4GFYUOpTaLl-gHbpHAtzSxgHg", onInitializedListener);
+            }
+        });
     }
 }
